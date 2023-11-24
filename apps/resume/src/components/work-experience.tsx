7608b4ac90eeb0fe261dc4c01da5a885adf9c7c4
rawnly <rawnly@users.noreply.github.com>
2023-11-25 00:21:07 +0100

    fixed type
    Signed-off-by: rawnly <rawnly@users.noreply.github.com>

--- apps/resume/src/components/work-experience.tsx
+++ apps/resume/src/components/work-experience.tsx
@@ -26,7 +26,9 @@ export default function WorkExperience(props: WorkExperience) {
     <div ref={ref} className="space-y-4">
       <div className="flex gap-4 justify-start items-end max-sm:items-start max-sm:flex-col">
         <div className="overflow-hidden relative w-12 h-12 rounded print:hidden">
-          <Image fill className="w-full h-full" src={props.image} />
+          {props.image && (
+            <Image fill className="w-full h-full" src={props.image} />
+          )}
         </div>
         <div className="flex flex-col justify-start items-start">
           <h3 className="font-mono text-lg font-bold sm:text-xl print:text-lg text-neutral-12">
